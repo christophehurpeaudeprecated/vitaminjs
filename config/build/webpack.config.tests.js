3bcb1e4bade3351b91f99d0f668c491f0ea1168b
Lucas <lucas@evaneos.com>
2016-09-09 15:45:26 +0200

    adding jsx support

--- config/build/webpack.config.tests.js
+++ config/build/webpack.config.tests.js
@@ -1,6 +1,7 @@
 import mergeWith from 'lodash.mergewith';
 import appConfig from '../index';
-import { config } from './webpack.config.common';
+import { vitaminResolve, concat } from '../utils';
+import { config, createBabelLoaderConfig } from './webpack.config.common';
 
 function testConfig(options) {
     return mergeWith({}, config(options), {
@@ -8,12 +9,21 @@ function testConfig(options) {
         output: {
             filename: 'tests.js',
         },
+        module: {
+            loaders: [
+                createBabelLoaderConfig('client'),
+                // The following loader will resolve the config to its final value during the build
+                {
+                    test: vitaminResolve('config/index'),
+                    loader: vitaminResolve('config/build/requireLoader'),
+                }],
+        },
         externals: {
             'react/addons': true,
             'react/lib/ExecutionEnvironment': true,
-            'react/lib/ReactContext': true
-        }
-    });
+            'react/lib/ReactContext': true,
+        },
+    }, concat);
 }
 
 module.exports = testConfig;
